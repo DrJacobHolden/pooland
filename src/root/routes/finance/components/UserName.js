eae8f18f6b0de5f678a08785ca50ca7f2247b2a7
Borig <borigthedwarf@gmail.com>
2020-06-07 11:01:00 +1200

    Reduce calls for usernames

--- src/root/routes/finance/components/UserName.js
+++ src/root/routes/finance/components/UserName.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useQuery } from "graphql-hooks";
 
 import { GET_USER } from "./queries";
 
+const cache = {};
+
 const UserName = ({ userId }) => {
+  if (cache[userId]) {
+    return <span>{cache[userId]}</span>;
+  }
+
+  return <UncachedUserName userId={userId} />;
+};
+
+const UncachedUserName = ({ userId }) => {
   const { data } = useQuery(GET_USER, { variables: { userId } });
+
+  useEffect(() => {
+    if (data) {
+      cache[userId] = data.users[0].name;
+    }
+  }, [data]);
 
   return <span>{data ? data.users[0].name : "loading..."}</span>;
 };
